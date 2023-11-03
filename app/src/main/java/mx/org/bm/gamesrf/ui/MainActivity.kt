52f453e40a6fbbce34890f2b2daddfebf3e0b1b9
OITD <oitd@banxico.org.mx>
2023-11-03 14:17:46 -0600

    CZR. Agrega botón para deter la música de fondo.

--- app/src/main/java/mx/org/bm/gamesrf/ui/MainActivity.kt
+++ app/src/main/java/mx/org/bm/gamesrf/ui/MainActivity.kt
@@ -1,14 +1,12 @@
 package mx.org.bm.gamesrf.ui
 
 import android.content.Intent
-import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import mx.org.bm.gamesrf.R
 import mx.org.bm.gamesrf.databinding.ActivityMainBinding
 import mx.org.bm.gamesrf.ui.fragments.FightersListFragment
 import mx.org.bm.gamesrf.util.SoundService
-import retrofit2.Retrofit
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,12 +19,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //startService(Intent(this, SoundService::class.java))
+        startService(Intent(this, SoundService::class.java))
 
         if(savedInstanceState == null){
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, FightersListFragment())
                 .commit()
+        }
+
+        binding.btnStopSound.setOnClickListener {
+            stopService(Intent(this, SoundService::class.java))
         }
     }
 }
